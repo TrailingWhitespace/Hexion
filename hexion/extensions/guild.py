7e2994114e4406f5c22c8c8c4eecaa57f3821cc5
TrailingWhitespace <flash1719@protonmail.com>
2024-10-03 08:55:40 +0530

    Add banner owo

--- hexion/extensions/guild.py
+++ hexion/extensions/guild.py
@@ -1,6 +1,7 @@
 import lightbulb
 from hexion import client
 from hexion.utils import create_embed
+import requests
 
 loader = lightbulb.Loader()
 
@@ -14,6 +15,8 @@ class Avatar(
     )
     user_id = lightbulb.string("userid", "The user ID duh.", default = None
     )
+    display = lightbulb.boolean("display", "To display the server specific url.", default = None)
+    size = lightbulb.integer("size", "The size of the avatar.", default = 4096)
 
     @lightbulb.invoke
     async def invoke(self, ctx):
@@ -23,8 +26,37 @@ class Avatar(
         if self.user_id:                                               user = await ctx.client.app.rest.fetch_user(self.user_id)
         if self.user and not self.user_id:
             user = self.user
-        em.set_image(user.avatar_url).set_author(
-            name=user.username, icon=user.avatar_url
+        em.color = user.accent_color
+        if not self.display:
+            av = user.make_avatar_url(size = self.size)
+        else:
+            av = user.display_avatar_url
+
+        if not av:
+            av = user.default_avatar_url
+        em.set_image(av).set_author(
+            name=user.username, icon=av
         )
         em.title = f"{user.display_name}'s avatar"
         await ctx.respond(embed=em)
+
+@client.register
+class Banner(
+        lightbulb.SlashCommand,
+        name = "banner",
+        description = "Get the banner of a user."
+):
+
+    user = lightbulb.user("user", "The user who's banner needs to be displayed.", default = None)
+
+    @lightbulb.invoke
+    async def invoke(self, ctx):
+        user = ctx.interaction.user if not self.user else self.user
+        user = await ctx.client.app.rest.fetch_user(user.id)
+        em = create_embed(ctx)
+        em.color = user.accent_color
+        em.set_image(user.banner_url).set_author(
+            name=user.username, icon=user.avatar_url
+        )
+        em.title = f"{user.display_name}'s banner"
+        await ctx.respond(embed = em)
