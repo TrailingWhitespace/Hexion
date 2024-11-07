543934c5ea3f97edc1b9202c51c247b3a78492a2
TrailingWhitespace <flash1719@protonmail.com>
2024-11-07 22:44:26 +0530

    DB and changes owo

--- hexion/core/client.py
+++ hexion/core/client.py
@@ -1,10 +1,14 @@
 import hikari
 import lightbulb
 from hexion import extensions, config
+from motor.motor_asyncio import AsyncIOMotorClient
 
 bot = hikari.GatewayBot(token=config.TOKEN)
-
 client = lightbulb.client_from_app(bot)
+
+db_client = AsyncIOMotorClient(config.MONGODB)
+db= db_client.get_database("hexion")
+misc_collection = db.misc
 
 @bot.listen(hikari.StartingEvent)
 async def on_starting(event):
